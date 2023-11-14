dd018da1bcf7b3cb052c3deaf42b3a2dd2cae639
jorgeemejia <jorgeemejia@csu.fullerton.edu>
2023-11-14 13:28:05 -0800

    Got Dpad in, need to add shine to buttons

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -6,6 +6,8 @@ import {
     Pressable
   } from 'react-native';
   import type { NativeStackScreenProps } from '@react-navigation/native-stack'
+  import Dpad from '../icons/dPad';
+  import LinearGradient from 'react-native-linear-gradient';
 
   type RootStackParamList ={
     Home: undefined;
@@ -19,26 +21,29 @@ function Home({navigation}: Props): JSX.Element {
 
   return (
     <View style={styles.screenContainer}>
-        <View style={styles.menuButtonContainer}>
-            <View style={styles.menuButtonContainerRow}>
-            <Pressable style={[styles.menuButton, {backgroundColor:'red'}]} onPress={() => navigation.navigate('Collection')}>
-                <Text style={styles.menuButtonText}>Collection</Text>
-            </Pressable>
-            </View>
-            <View style={styles.menuButtonContainerRow}>
-                <Pressable style={[styles.menuButton, {backgroundColor:'green'}]} onPress={() => navigation.navigate('Home')}>
-                    <Text style={styles.menuButtonText}>Wishlist</Text>
+        <View style={styles.allGameButtonsContainer}>
+            <Dpad style={styles.dPad}/>
+            <View style={styles.menuButtonsContainer}>
+                <View style={styles.menuButtonContainerRow}>
+                <Pressable style={[styles.menuButton, {backgroundColor:'#FF0000'}]} onPress={() => navigation.navigate('Collection')}>
+                    <Text style={styles.menuButtonText}>Collection</Text>
                 </Pressable>
-                <View> 
                 </View>
-                <Pressable style={[styles.menuButton, {backgroundColor:'blue'}]} onPress={() => navigation.navigate('Home')}>
-                    <Text style={styles.menuButtonText}>Explore</Text>
-                </Pressable>
-            </View>
-            <View style={styles.menuButtonContainerRow}>
-                <Pressable style={[styles.menuButton, {backgroundColor:'yellow'}]} onPress={() => navigation.navigate('Home')}>
-                    <Text style={styles.menuButtonText}>Review</Text>
-                </Pressable>
+                <View style={styles.menuButtonContainerRow}>
+                    <Pressable style={[styles.menuButton, {backgroundColor:'#00FF00'}]} onPress={() => navigation.navigate('Home')}>
+                        <Text style={styles.menuButtonText}>Wishlist</Text>
+                    </Pressable>
+                    <View> 
+                    </View>
+                    <Pressable style={[styles.menuButton, {backgroundColor:'#1C5D99'}]} onPress={() => navigation.navigate('Home')}>
+                        <Text style={styles.menuButtonText}>Explore</Text>
+                    </Pressable>
+                </View>
+                <View style={styles.menuButtonContainerRow}>
+                    <Pressable style={[styles.menuButton, {backgroundColor:'#FFFF00'}]} onPress={() => navigation.navigate('Home')}>
+                        <Text style={styles.menuButtonText}>Review</Text>
+                    </Pressable>
+                </View>
             </View>
         </View>
     </View>
@@ -47,9 +52,20 @@ function Home({navigation}: Props): JSX.Element {
 
 const styles = StyleSheet.create({
     screenContainer : {
-        flex: 1
+        flex: 1,
+        backgroundColor: '#B08FC7'
     },
-    menuButtonContainer : {
+    allGameButtonsContainer : {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 20
+    },
+    dPad : {
+        width: 100,
+        height: 100
+    },
+    menuButtonsContainer : {
     },
     menuButtonContainerRow : {
       flexDirection: 'row',
@@ -58,7 +74,9 @@ const styles = StyleSheet.create({
     menuButton : {
         padding: 10,
         borderRadius: 10,
-        margin: 10
+        margin: 10,
+        borderRightColor: 'black',
+        borderRightWidth: 5,
     },
     menuButtonText : {
         fontSize: 20,
